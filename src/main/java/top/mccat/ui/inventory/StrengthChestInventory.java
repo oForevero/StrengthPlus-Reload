5d98a6487d5dea1042f921f62e1aa06d71fb5bb9
Distance <740585947@qq.com>
2022-05-18 15:41:10 +0800

    初步实现通知模块

--- src/main/java/top/mccat/ui/inventory/StrengthChestInventory.java
+++ src/main/java/top/mccat/ui/inventory/StrengthChestInventory.java
@@ -9,21 +9,28 @@ import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * @author Distance
  * @date 2022/5/17 17:56
  */
 public class StrengthChestInventory implements DoubleChestInventory {
-
+    private int maxStackSize;
+    private List<ItemStack> itemStacks;
+    private List<HumanEntity> humanEntities;
+    private final ItemStack airStack = new ItemStack(Material.AIR);
+    /**
+     * 未使用
+     */
     @Override
     public Inventory getLeftSide() {
         return null;
     }
 
+    /**
+     * 未使用
+     */
     @Override
     public Inventory getRightSide() {
         return null;
@@ -36,29 +43,35 @@ public class StrengthChestInventory implements DoubleChestInventory {
 
     @Override
     public int getMaxStackSize() {
-        return 0;
+        return maxStackSize;
     }
 
     @Override
-    public void setMaxStackSize(int i) {
-
+    public void setMaxStackSize(int size) {
+        maxStackSize = size;
     }
 
     @Override
     public ItemStack getItem(int i) {
-        return null;
+        return itemStacks.get(i);
     }
 
     @Override
     public void setItem(int i, ItemStack itemStack) {
-
+        itemStacks.set(i,itemStack);
     }
 
+    /**
+     * 未使用
+     */
     @Override
     public HashMap<Integer, ItemStack> addItem(ItemStack... itemStacks) throws IllegalArgumentException {
         return null;
     }
 
+    /**
+     * 未使用
+     */
     @Override
     public HashMap<Integer, ItemStack> removeItem(ItemStack... itemStacks) throws IllegalArgumentException {
         return null;
@@ -66,17 +79,31 @@ public class StrengthChestInventory implements DoubleChestInventory {
 
     @Override
     public ItemStack[] getContents() {
-        return new ItemStack[0];
+        ItemStack[] itemStackArray = new ItemStack[54];
+        return itemStacks.toArray(itemStackArray);
     }
 
     @Override
     public void setContents(ItemStack[] itemStacks) throws IllegalArgumentException {
-
+        this.itemStacks = Arrays.asList(itemStacks);
     }
 
     @Override
     public ItemStack[] getStorageContents() {
-        return new ItemStack[0];
+        ItemStack[] stacks;
+        if(!itemStacks.contains(airStack)){
+            stacks = new ItemStack[0];
+            return stacks;
+        }else {
+            List<ItemStack> stackList = new ArrayList<>();
+            for(ItemStack stack : itemStacks){
+                if (stack.getType() == Material.AIR){
+                    stackList.add(stack);
+                }
+            }
+            stacks = new ItemStack[stackList.size()];
+            return stackList.toArray(stacks);
+        }
     }
 
     @Override
@@ -86,22 +113,32 @@ public class StrengthChestInventory implements DoubleChestInventory {
 
     @Override
     public boolean contains(Material material) throws IllegalArgumentException {
+        ItemStack stack = new ItemStack(material);
+        if(itemStacks.contains(stack)){
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean contains(ItemStack itemStack) {
+        return contains(itemStack.getType());
+    }
+
+    @Override
+    public boolean contains(Material material, int amount) throws IllegalArgumentException {
+        ItemStack stack = new ItemStack(material);
+        if(itemStacks.contains(stack)){
+            if(stack.getAmount() == amount){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean contains(Material material, int i) throws IllegalArgumentException {
-        return false;
-    }
-
-    @Override
-    public boolean contains(ItemStack itemStack, int i) {
-        return false;
+    public boolean contains(ItemStack itemStack, int amount) {
+        return contains(itemStack.getType(),amount);
     }
 
     @Override
@@ -111,39 +148,80 @@ public class StrengthChestInventory implements DoubleChestInventory {
 
     @Override
     public HashMap<Integer, ? extends ItemStack> all(Material material) throws IllegalArgumentException {
-        return null;
+        HashMap<Integer, ItemStack> map = new HashMap<>(54);
+        for(int i = 0; i < itemStacks.size(); i++){
+            ItemStack stack = itemStacks.get(i);
+            if(stack.getType() == material) {
+                map.put(i,stack);
+            }
+        }
+        return map;
     }
 
     @Override
     public HashMap<Integer, ? extends ItemStack> all(ItemStack itemStack) {
-        return null;
+        HashMap<Integer, ItemStack> map = new HashMap<>(54);
+        for(int i = 0; i < itemStacks.size(); i++){
+            ItemStack stack = itemStacks.get(i);
+            if(stack.equals(itemStack)) {
+                map.put(i,stack);
+            }
+        }
+        return map;
     }
 
     @Override
     public int first(Material material) throws IllegalArgumentException {
-        return 0;
+        for(int i = 0; i < itemStacks.size(); i++){
+            ItemStack stack = itemStacks.get(i);
+            if(stack.getType() == material){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int first(ItemStack itemStack) {
-        return 0;
+        for(int i = 0; i < itemStacks.size(); i++){
+            ItemStack stack = itemStacks.get(i);
+            if(stack.equals(itemStack)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int firstEmpty() {
-        return 0;
+        for(int i = 0; i < itemStacks.size(); i++){
+            ItemStack stack = itemStacks.get(i);
+            if(stack.equals(airStack)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmpty() {
+        if(itemStacks == null || itemStacks.size()==0) {
+            return true;
+        }
         return false;
     }
 
+    /**
+     * 未使用
+     */
     @Override
     public void remove(Material material) throws IllegalArgumentException {
 
     }
 
+    /**
+     * 未使用
+     */
     @Override
     public void remove(ItemStack itemStack) {
 
@@ -151,12 +229,12 @@ public class StrengthChestInventory implements DoubleChestInventory {
 
     @Override
     public void clear(int i) {
-
+        itemStacks.set(i, airStack);
     }
 
     @Override
     public void clear() {
-
+        itemStacks.clear();
     }
 
     @Override
@@ -187,5 +265,9 @@ public class StrengthChestInventory implements DoubleChestInventory {
     @Override
     public Location getLocation() {
         return null;
+    }
+
+    public void setItemStacks(List<ItemStack> itemStacks) {
+        this.itemStacks = itemStacks;
     }
 }
