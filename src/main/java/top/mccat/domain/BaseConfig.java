f228e0d54fbfbb3a86f1c5a909eda93a0d310354
Distance <740585947@qq.com>
2022-05-19 16:22:14 +0800

    初步实现强化ui模块

---
+++ src/main/java/top/mccat/domain/BaseConfig.java
@@ -0,0 +1,19 @@
+package top.mccat.domain;
+
+/**
+ * @author Distance
+ * @date 2022/5/19 16:18
+ */
+public abstract class BaseConfig {
+    /**
+     * 配置文件名
+     * @return 自己的配置文件名
+     */
+    abstract String configFileName();
+
+    /**
+     * 当出现加载失败的错误消息
+     * @return 错误消息枚举
+     */
+    abstract String loadErrorMsg();
+}
